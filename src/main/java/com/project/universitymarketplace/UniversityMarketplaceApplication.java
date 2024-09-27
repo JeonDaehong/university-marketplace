0e0f8526999bd62eca3728fa0551cbc510533c92
JeonDaehong <daehong770@gmail.com>
2024-09-27 22:40:13 +0900

    init commit

---
+++ src/main/java/com/project/universitymarketplace/UniversityMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.project.universitymarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityMarketplaceApplication.class, args);
+    }
+
+}
