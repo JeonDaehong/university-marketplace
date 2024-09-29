1e741f976a0fe54551eca02d49550537b5201c86
JeonDaehong <daehong770@gmail.com>
2024-09-29 22:59:59 +0900

    [#1] : feat - daehong: CurrentUser Check Annotation Develop

---
+++ src/main/java/com/project/universitymarketplace/common/annotation/CurrentUser.java
@@ -0,0 +1,9 @@
+package com.project.universitymarketplace.common.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CurrentUser {
+}
