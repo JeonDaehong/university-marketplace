16e011f4814e955982d931a389a937ab59244727
JeonDaehong <daehong770@gmail.com>
2024-10-01 19:04:14 +0900

    feat: daehong >>> add common URI utility class

---
+++ src/main/java/com/project/universitymarketplace/common/common/CommonURI.java
@@ -0,0 +1,12 @@
+package com.project.universitymarketplace.common.common;
+
+public class CommonURI {
+
+    // private
+    private static final String COMMON_URI = "/university-marketplace";
+
+    // public
+    public static final String LOGIN_API = COMMON_URI + "/api/login";
+    public static final String USER_API = COMMON_URI + "/api/user";
+
+}
