d71ea2facd5f06e43b2e8be56b91d1cb7afddd8e
JeonDaehong <daehong770@gmail.com>
2024-10-01 19:05:14 +0900

    feat: daehong >>> develop base structure for User entity

---
+++ src/main/java/com/project/universitymarketplace/domain/User/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.project.universitymarketplace.domain.User.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.project.universitymarketplace.common.common.CommonURI.LOGIN_API;
+import static com.project.universitymarketplace.common.common.CommonURI.USER_API;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping(USER_API)
+public class UserController {
+
+}
